15962392fe0020fc00a0ad8f467cc7fbc51c674f
Kseg110 <kylseg@icloud.com>
2025-02-17 22:58:02 -0500

    initializing screen and started infinite loop for screen background

--- main.py
+++ main.py
@@ -4,6 +4,14 @@
 import pygame
 from constants import *
 
+pygame.get_init() #initializing pygame
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #set GUI window/ format size
+
+color = (r,g,b)
+while True:
+	screen.fill(color, rect=None, special_flags=0) #infinite while loop to fill screen with solid black color
+
 def main():
 	print( "Starting asteroids!")
 	print("Screen width: 1280")
